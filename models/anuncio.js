8f9286985e036b230c6b15ddd32229398fcde617
carolina-CortezFreudenthal <carolicortfreu@gmail.com>
2023-01-07 00:10:37 +0100

    agregar filtros

--- models/anuncio.js
+++ models/anuncio.js
@@ -10,6 +10,16 @@ const AnuncioSchema = new Schema({
     tags: [String],
   });
 
+AnuncioSchema.statics.consulta = (paginacion /* {skip, limit} */, filtros) => {
+  const query = AnuncioModel.find(filtros);
+  query.skip(paginacion.skip);
+  query.limit(paginacion.limit);
+  query.sort(paginacion.sort);
+  // query.select(campos);
+  // query.sort(sort);
+  return query.exec()
+}
+
 // Creacion del modelo usando el esquema
 let AnuncioModel = mongoose.model('Anuncio', AnuncioSchema);
 module.exports = AnuncioModel;
--- routes/api/anuncios.js
+++ routes/api/anuncios.js
@@ -8,7 +8,53 @@ var router = express.Router();
 
 router.get('/', async (req, res, next) => {
   try {
-    const anuncios = await AnuncioModel.find();
+    // filtros
+    const nombre = req.query.nombre;
+    const venta = req.query.venta;
+    const precio = req.query.precio;
+    const tags = req.query.tags;
+
+    const paginacion = {
+      skip : req.query.skip,
+      limit : req.query.limit,
+      sort : req.query.sort,
+    }
+
+    const filtros = {};
+    if (nombre) { // /api/anuncios?name=Bici
+      filtros.nombre = new RegExp('^' + nombre, "i");
+    }
+
+    if (venta) { // /api/anuncios?venta=true
+      filtros.venta = venta;
+    }
+
+    if (tags) {
+      filtros.tags = { '$in': tags }
+    }
+
+    if (precio) { // /api/anuncions?precio=10-50 o /api/anuncions?precio=10- o /api/anuncions?precio=-50 o /api/anuncions?precio=50
+      let range = precio.split("-");
+    
+      // Si solo tenemos uno => /api/anuncions?precio=50
+      // El filtro verifica que el precio sea igual
+      if (range.length == 1) filtros.precio = precio;
+      // Si el primer elemento es vacio => /api/anuncions?precio=-50
+      // El filtro verifica que el precio sea menor a range[1]
+      else if (range[0] == "") filtros.precio = { '$lte': range[1] }
+      // Si el segundo elemento es vacio => /api/anuncions?precio=50-
+      // El filtro verifica que el precio sea mayor a range[9]
+      else if (range[1] == "") filtros.precio = { '$gte': range[0] }
+      // Si los dos elementos existen => /api/anuncions?precio=10-50
+      // El filtro verifica que el precio este entre los valores
+      else filtros.precio = { '$gte': range[0], '$lte': range[1] }
+    }
+
+    // if (age) { // /api/anuncios?age=32
+    //   filtro.age = age;
+    // }
+
+    const anuncios = await AnuncioModel.consulta(paginacion, filtros);
     res.json({ data: anuncios });
   } catch(err) {
     next(err)
@@ -16,3 +62,4 @@ router.get('/', async (req, res, next) => {
 });
 
 module.exports = router;
+
